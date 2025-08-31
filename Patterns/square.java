792ec77984f62c8fbb86af752d63009b72983168
vijendra2002 <vijendrasinghparmar553@gmail.com>
2025-08-31 16:34:19 +0530

    pattern added

---
+++ Patterns/square.java
@@ -0,0 +1,20 @@
+package Patterns;
+
+import java.util.*;
+
+public class square {
+  
+  public static void main(String[] args) {
+    int n = 4;
+    int m = 5;
+
+    // outer loop //
+    for(int i=1; i<=n; i++ ) {
+      // inner loop //
+    for(int j=1; j<=m; j++) {
+      System.out.print("*");
+     }
+     System.out.println("");
+   }
+  }
+}
